e5d17ab867ddbb5b31305696c3cddad44ced4da8
Patrick Gaissert <patrick.gaissert@maibornwolff.de>
2019-04-12 16:33:04 +0200

    presentation

---
+++ DearDiary/Presentation/DiaryListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryListViewController.swift
+//  DearDiary
+//
+//  Created by Patrick Gaissert on 02.04.19.
+//  Copyright © 2019 MaibornWolff GmbH. All rights reserved.
+//
+
+import UIKit
+
+class DiaryListViewController: UIViewController {
+    @IBOutlet var quoteTextLabel: UILabel!
+    @IBOutlet var quoteAuthorLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+}
